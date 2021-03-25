525f30227bb53522b09d6845be214089591b76f3
ouyangfan <ouyf@tiansu-china.com>
2021-03-25 17:26:44 +0800

    new-operator

---
+++ context/new.js
@@ -0,0 +1,26 @@
+function _new(ctor, ...args) {
+    if (typeof ctor !== 'function') {
+        throw new TypeError('ctor must be a function');
+    }
+
+    let obj = new Object();
+    obj.__proto__ = Object.create(ctor.prototype);
+    let res = ctor.apply(obj, [...args]);
+
+    let isObject = typeof res === 'object' && res !== null;
+    let isFunction = typeof res === 'function';
+
+    return isObject || isFunction ? res : obj;
+}
+
+function test() {
+    this.name = 'test';
+}
+
+test.prototype.getName = function () {
+    console.log(this.name);
+}
+
+let res = _new(test, 'c');
+console.log(res);
+res.getName();
